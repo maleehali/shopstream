f7a74ca0b783db6ca8a07db7af9e6e75f67c080f
Maleeha <162085935+maleehali@users.noreply.github.com>
2024-11-06 14:39:53 -0500

    debugged and editted

--- models/index.js
+++ models/index.js
@@ -3,7 +3,12 @@ const Product = require('./Product');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-Product.belongsTo(Category, { foreignKey: 'category_id' });
+// Add onDelete: 'CASCADE' to automatically delete associated products when a category is deleted
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+
 Category.hasMany(Product, { foreignKey: 'category_id' });
 Product.belongsToMany(Tag, { through: ProductTag, foreignKey: 'product_id' });
 Tag.belongsToMany(Product, { through: ProductTag, foreignKey: 'tag_id' });
